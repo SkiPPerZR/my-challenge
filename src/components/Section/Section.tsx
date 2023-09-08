b9838b88c5d5f02f062e2fd927a0ee1023451da7
Paul Molodtsov <skipperzr@gmail.com>
2023-09-08 13:55:43 +0300

    Add Section block with title and cards

--- src/components/Section/Section.tsx
+++ src/components/Section/Section.tsx
@@ -1,5 +1,6 @@
 import React, {FC, useState} from 'react';
 import './Section.scss'
+import Card, {CardVariant} from '../../shared/card/Card';
 
 interface MarkersProps {
     marker_name: string;
@@ -9,23 +10,42 @@ interface SectionProps {
     icon: any;
     name: string;
     count: number;
-    filters: MarkersProps[];
+    // filters: MarkersProps[];
 }
 
-const Section:FC<SectionProps> = ({icon,name,count,filters}) => {
+const Section:FC<SectionProps> = ({icon,name,count}) => {
     const [markers, setMarkers] = useState([]);
 
     return (
         <div className='Section'>
-            <div className='SectionTitle'>
-                <img src={icon} alt="Icon" />
-                <span>{name}</span>
-                <div className='SectionTitleCount'>
-                    <span><span>{count}</span> челленджей</span>
+            <div className='SectionHeader'>
+                <div className='SectionHeaderTitle'>
+                    <img src={icon} alt="Icon" />
+                    <span className='text-18 semibold'>{name}</span>
+                    <div className='SectionHeaderTitleCount'>
+                        <span className='text-14 regular'>{count} челленджей</span>
+                    </div>
+                </div>
+                <div className='SectionHeaderFilters'>
+                    <button className='SectionHeaderFiltersButton text-14 regular'>Все</button>
+                    <button className='SectionHeaderFiltersButton text-14 regular'>Dota 2</button>
+                    <button className='SectionHeaderFiltersButton text-14 regular'>Counter-Strike</button>
+                    <button className='SectionHeaderFiltersButton text-14 regular'>League of legends</button>
+                    <button className='SectionHeaderFiltersButton text-14 regular'>Valorant</button>
+                    <button className='SectionHeaderFiltersButton text-14 regular'>Minecraft</button>
+                </div>
+                <div className='SectionHeaderContent'>
+                    <span className='text-18 regular'>Посмотреть всe</span>
+                    <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M8.90625 19.9201L15.4263 13.4001C16.1963 12.6301 16.1963 11.3701 15.4263 10.6001L8.90625 4.08008" stroke="#102B32" stroke-width="1.5" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"/>
+                    </svg>
                 </div>
             </div>
-            <div className='SectionFilters'>
-                <button className='SectionFiltersButton'></button>
+            <div className='SectionChallenges'>
+                <Card variant={CardVariant.blogger}/>
+                <Card variant={CardVariant.premium}/>
+                <Card variant={CardVariant.based}/>
+                <Card variant={CardVariant.standart}/>
             </div>
         </div>
     );
--- src/pages/main/Main.tsx
+++ src/pages/main/Main.tsx
@@ -2,7 +2,8 @@ import React from 'react';
 import Header from '../../components/Header/Header';
 import Sidebar from '../../components/Sidebar/Sidebar';
 import Card, {CardVariant} from '../../shared/card/Card'
-
+import Section from '../../components/Section/Section';
+import icon from '../../img/SidebarLg/GamesLg.svg'
 
 const Main = () => {
     return (
@@ -10,7 +11,7 @@ const Main = () => {
             <Sidebar />
             <div className='Container'>
                 <Header />
-                
+                <Section icon={icon} name='Видеоигры' count={12415}/>
             </div>
         </div>
     );
