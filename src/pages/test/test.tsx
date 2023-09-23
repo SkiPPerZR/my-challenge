4a92dcae2320370c3a3b73f2048288ffb5be2f83
SkipperZR <skipperzr@gmail.com>
2023-09-23 20:10:04 +0300

    add time and bet element

--- src/pages/test/test.tsx
+++ src/pages/test/test.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Sidebar from '../../components/Sidebar/Sidebar';
 import Header from '../../components/Header/Header';
 import ButtonAccept from '../../shared/buttons/ButtonAccept';
+import TimeAndBet from '../../shared/timeAndBet/TimeAndBet';
 
 const Test = () => {
     return (
@@ -9,6 +10,7 @@ const Test = () => {
             <Sidebar backbutton={0}/>
             <div className='Container'>
                 <Header login={1}/>
+                <TimeAndBet/>
                 <ButtonAccept/>
             </div>
         </div>
--- src/shared/timeAndBet/TimeAndBet.tsx
+++ src/shared/timeAndBet/TimeAndBet.tsx
@@ -1,10 +1,22 @@
 import React from 'react';
 import './TimeAndBet.scss'
 
+import gold from '../../img/Gold-2.svg'
+
 const TimeAndBet = () => {
     return (
-        <div>
-            
+        <div className='TimeAndBet'>
+            <div className='TimeAndBetTimer'>
+                <span className='text-17 semibold'>23 часа</span>
+                <p className='text-14 regular'>Осталось времени</p>
+            </div>
+            <div className='TimeAndBetPrise'>
+                <div>
+                    <span className='text-17 semibold'>760</span>
+                    <img src={gold} alt="Золото" />
+                </div>
+                <p className='text-14 regular'>Выигрыш</p>
+            </div>             
         </div>
     );
 };
